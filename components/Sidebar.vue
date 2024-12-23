ad57e461062b75f07c918499a46ed10b5ed55e3c
NanPA2002 <nuichaiyanan223@gmail.com>
2024-12-23 14:21:57 +0700

    update sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <div class="bg-[#F7F1FD] p-[20px] w-[300px]  rounded-[20px] ">
+    <div>
+      <img class="object-cover " src="https://img2.pic.in.th/pic/logo_manage.png" alt="">
+    </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
+  <div class="flex gap-5 py-[60px] ps-[30px]">
+    <Sidebar />
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup>
+import Sidebar from '~/components/Sidebar.vue';
+import Sildesbar from '~/components/Sidebar.vue';
+
 
 </script>
 
