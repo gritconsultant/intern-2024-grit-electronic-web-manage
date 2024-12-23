21a2276bd8505b7fffcdc0d6db353b3e278a4ae2
NanPA2002 <nuichaiyanan223@gmail.com>
2024-12-23 13:36:37 +0700

    update configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig:{
+    public: {
+      BASE_URL: '',
+    }
+  },
+  app: {
+    head: {
+      title: 'grit Electronix',
+      script: [
+        {
+          src: 'https://kit.fontawesome.com/02a8dd6235.js',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
+  css: ['@/assets/css/main.css','~/assets/scss/main.scss'],
+  modules: ['@pinia/nuxt'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+        },
+      },
+    },
+  },
+  postcss: {
+      plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      },
+  },
+});
