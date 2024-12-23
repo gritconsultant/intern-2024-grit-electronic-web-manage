74014fb3c1a7322b8921f0b77b45d06bb1b93fe5
NanPA2002 <nuichaiyanan223@gmail.com>
2024-12-23 23:12:07 +0700

    update indexmanage

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,86 @@
 <template>
   <div class="flex gap-5 w-full">
-    <div class="bg-[#765798]/60 p-5 w-3/4 rounded-[20px]">
+    <div class="bg-[#765798]/60 p-5 w-3/4 rounded-[20px] flex flex-col gap-5">
       <div class="flex gap-5">
-        <div class="bg-white rounded-[50px] w-full p-5 flex justify-between  ">
-          <div class="pl-5 w-full bg-slate-400 ">
+        <div class="bg-white rounded-[50px] w-full p-2 flex justify-between">
+          <div class="pl-5 w-5/6 bg-slate-400">
             <i class="fa-solid fa-calendar-days"></i>
-            selectวัน</div>
-          <div class="pr-5 w-full flex justify-end bg-slate-300">slect date</div>
+          </div>
+          <div
+            class="w-1/6 flex justify-center text-center border-2 rounded-[20px] border-[#765798]"
+          >
+            <select name="" id="" class="border-white">
+              <option value="volvo">ปี</option>
+              <option value="volvo">เดือน</option>
+              <option value="volvo">สัปาดห์</option>
+              <option value="volvo">วัน</option>
+            </select>
+          </div>
         </div>
         <div class="w-1/8 bg-black flex items-center">
           <i class="fa-solid fa-bell text-yellow-400"></i>
         </div>
+      </div>
+      <div class="flex gap-5">
+        <div class="rounded-[20px] p-2 w-1/4 grid gap-5 bg-white">
+          <div class="flex gap-2">
+            <div
+              class="p-2 rounded-[100px] w-[35px] flex justify-center bg-white"
+            >
+              <i class="fa-solid fa-comment-dollar"></i>
+            </div>
+            <span class="bg-slate-500 w-full flex items-center">ชื่อ</span>
+          </div>
+          <h1 class="text-center font-bold text-lg text-red-600 truncate">฿</h1>
+        </div>
+        <div class="rounded-[20px] p-2 w-1/4 grid gap-5 bg-white">
+          <div class="flex gap-2">
+            <div
+              class="p-2 rounded-[100px] w-[35px] flex justify-center bg-white"
+            >
+              <i class="fa-solid fa-comment-dollar"></i>
+            </div>
+            <span class="bg-slate-500 w-full flex items-center">ชื่อ</span>
+          </div>
+          <h1 class="text-center font-bold text-lg text-red-600 truncate">฿</h1>
+        </div>
+        <div class="rounded-[20px] p-2 w-1/4 grid gap-5 bg-white">
+          <div class="flex gap-2">
+            <div
+              class="p-2 rounded-[100px] w-[35px] flex justify-center bg-white"
+            >
+              <i class="fa-solid fa-comment-dollar"></i>
+            </div>
+            <span class="bg-slate-500 w-full flex items-center">ชื่อ</span>
+          </div>
+          <h1 class="text-center font-bold text-lg text-red-600 truncate">฿</h1>
+        </div>
+        <div class="rounded-[20px] p-2 w-1/4 grid gap-5 bg-white">
+          <div class="flex gap-2">
+            <div
+              class="p-2 rounded-[100px] w-[35px] flex justify-center bg-white"
+            >
+              <i class="fa-solid fa-comment-dollar"></i>
+            </div>
+            <span class="bg-slate-500 w-full flex items-center">ชื่อ</span>
+          </div>
+          <h1 class="text-center font-bold text-lg text-red-600 truncate">฿</h1>
+        </div>
+      </div>
+      <div
+        class="p-5 bg-white rounded-[20px] text-black/70 flex flex-col gap-5"
+      >
+        <div class="flex justify-between">
+          <div class="bg-slate-500">
+            <h1>สรุปรายได้</h1>
+            <span>0000</span>
+          </div>
+          <div>
+            <div>selectorrewrewrwrewrw</div>
+            <div class="flex justify-end">selectoryaer</div>
+          </div>
+        </div>
+        <div>graf </div>
       </div>
     </div>
     <div class="bg-black w-1/4 mr-2">asdd</div>
